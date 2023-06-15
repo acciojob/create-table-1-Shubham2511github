a63332f58bd421c42c11be5baf9d1bffe384aed7
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-15 22:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
+	const table = document.getElementById("sampleTable");
+	var newRow = table.insertRow(0);
+	let newCell1 = newRow.insertCell(0);
+	let newCell2 = newRow.insertCell(1);
+	newCell1.innerHTML = "New Cell1";
+	newCell2.innerHTML = "New Cell2";
   
   
 }
